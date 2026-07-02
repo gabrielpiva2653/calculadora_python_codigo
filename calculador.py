99ace781adc4077cce0193f38301831e5e673250
gabrielpiva2653 <gabriel.lerenasilva@gmail.com>
2026-07-01 22:04:48 -0300

    código_calculadora
    Aqui temos a construção de todo código.

---
+++ calculador.py
@@ -0,0 +1,78 @@
+nome =input("Digite seu nome: ")
+print(nome)
+
+soma=input("Vamos realizar essa soma")
+print(soma)
+
+x=int(input("Digite o valor de x: "))
+print(x)
+
+y=int(input("Digite o valor de y: "))
+print(y)
+
+soma = x + y
+print(soma)
+
+mensagem = ("O resultado dessa soma é:",soma)
+print(mensagem)
+
+if soma > 10:
+  print("O resultado da soma é maior que 10")
+else:
+  print("O resultado da soma é menor que 10")
+
+
+
+subtração=input("Vamos realizar essa subtração")
+print(subtração)
+
+x=int(input("Digite o valor de x: "))
+print(x)
+
+y=int(input("Digite o valor de y: "))
+print(y)
+
+sub = x - y
+print(sub)
+
+mensagem = ("O resultado da subtração é:",sub)
+print(mensagem)
+
+
+
+multiplicação=input("Vamos realizar essa multiplicação")
+print(multiplicação)
+
+x=int(input("Digite o valor de x: "))
+print(x)
+
+y=int(input("Digite o valor de y: "))
+print(y)
+
+mult = x * y
+print(mult)
+
+mensagem = ("O resultado da operação é:",mult)
+print(mensagem)
+
+if mult > 100:
+  print("O resultado dessa multiplicação é maior que 100")
+else:
+  print("O resultado dessa multiplicação é menor que 100")
+
+x=int(input("Digite o valor de x: "))
+print(x)
+
+y=int(input("Digite o valor de y: "))
+print(y)
+
+div = x / y
+print(div)
+
+mensagem = ("O resultado da divisão é:",div)
+print(mensagem)
+
+if div > 100:
+  print("O resultado é maior que 6")
+else:
+  print("O resultado é menor que 6")
